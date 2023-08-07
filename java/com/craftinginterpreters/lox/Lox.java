498a6da9582e2edf6cb75065a366ff9ef66e1b37
Luv <luv.vermani@gmail.com>
2023-08-08 03:32:20 +0530

    added basic arithmetic interpreter ch 7

--- java/com/craftinginterpreters/lox/Lox.java
+++ java/com/craftinginterpreters/lox/Lox.java
@@ -9,8 +9,10 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
-
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
+
     public static void main(String[] args) throws IOException {
         if(args.length > 1) {
             System.out.println("Usage: jlox[script]");
@@ -26,7 +28,9 @@ public class Lox {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
 
+        // Indicate an error in the exit code.
         if(hadError) System.exit(65);
+        if(hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -49,16 +53,22 @@ public class Lox {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
         Parser parser = new Parser(tokens);
-        Expr expr = parser.parse();
+        Expr expression = parser.parse();
 
         // Stop if there is a syntax error.
         if (hadError) return;
 
-        System.out.println(new AstPrinter().print(expr));
+        // System.out.println(new AstPrinter().print(expression));
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String message) {
         report(line, "", message);
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line" + error.token.line + "]");
+        hadRuntimeError = true;
     }
 
     private static void report(int line, String where, String message) {
